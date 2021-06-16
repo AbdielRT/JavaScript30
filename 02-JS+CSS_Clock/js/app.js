dd276ae18a72a20cb8e9cb7c1a5e3c557cb62c7d
Abdiel <polito.abs@gmail.com>
2021-06-16 11:17:26 +0200

    day 2 JS30

---
+++ 02-JS+CSS_Clock/js/app.js
@@ -0,0 +1,21 @@
+// Evaluate function every second
+setInterval(function(){
+// Retrieve current full date
+	var date = new Date();
+
+// and stock it in separate variables for hh:mm:ss 
+	var hh = date.getHours();
+	var mm = date.getMinutes();
+	var ss = date.getSeconds();
+// Adds to the css the transformation rotate, adjusted for the 
+// corresponding degrees depending on hours, minutes and secs.
+
+// Version with hour hand fixed in current hour position the whole hour
+	$('.hour-hand').css({'transform':`rotate(${hh * 30 + 90}deg)`});
+
+// Reproduces mechanical clock hour hand 
+//	$('.hour-hand').css({'transform':`rotate(${hh * 30 + 90 + mm/2}deg)`});
+
+	$('.min-hand').css({'transform':`rotate(${mm * 6 + 90}deg)`});
+	$('.second-hand').css({'transform':`rotate(${ss * 6 + 90}deg)`});
+}, 1000);
