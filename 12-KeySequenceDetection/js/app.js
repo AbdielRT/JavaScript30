c2f5860c3f0d22dfd68a923bbe1fac7cd5b51334
Abdiel <polito.abs@gmail.com>
2021-06-17 17:05:02 +0200

    comments on js code

--- 12-KeySequenceDetection/js/app.js
+++ 12-KeySequenceDetection/js/app.js
@@ -1,17 +1,24 @@
 const motPasse = 'unicornio';
+// initiliase empty array
 var attemps = [];
 
+// add event listener on a key press
 document.onkeydown = function (e){
+	// adds the key output to the array
 	attemps.push(e.key);
+	// shows dynamic array as a key is pressed
 	console.log(attemps);
 
+	// Once the length of the attemps array matches the length of the password
 	while (attemps.length >= motPasse.length){
-		
+		// verifies is the password has been decoded
 		if (attemps.join('') == motPasse){
-			cornify_add();
+			cornify_add(); // then displays unicorns
 			console.log('You got it!');
 		}
-		attemps.splice(0,1).push(e.key);		
+		// otherwise, it displaces the first element in the array to receive a 
+		// new one in the last entry.
+		attemps.splice(0,1).push(e.key);	
 	}
 
 }
