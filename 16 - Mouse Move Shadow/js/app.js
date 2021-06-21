b6fc7e75d05bce8cfc16b393e9c2abe880edd1bc
Abdiel <polito.abs@gmail.com>
2021-06-21 12:00:30 +0200

    shadows reacting to mouse motion

--- 16 - Mouse Move Shadow/js/app.js
+++ 16 - Mouse Move Shadow/js/app.js
@@ -1,11 +1,14 @@
-
+// Retrieve containers of interest
 const hero = document.querySelector('.hero');
 const movable = document.querySelector('#movable');
 
+// function deployed on mouse movement 
 hero.onmousemove = function (e){
+	// recentering pointer to movable container 
+	// position, displace by half
 	var x = e.pageX - movable.offsetLeft*1.5;
 	var y = e.pageY - movable.offsetTop*1.5;
-
+// shadows to react to pointer position 
 	movable.style.textShadow = `
 		${x}px ${y}px 0 rgba(255,140,0,0.7),
 		${-x}px ${y}px 0 rgba(255,20,147,0.7),
