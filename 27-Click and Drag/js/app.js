7c7a4933c6527b4cc4bf382f2b40289b7c4a78f4
Abdiel <polito.abs@gmail.com>
2021-06-21 17:17:34 +0200

    drag active but still very sensible to mouse move

--- 27-Click and Drag/js/app.js
+++ 27-Click and Drag/js/app.js
@@ -1,9 +1,24 @@
 const slider = document.querySelector('.items');
+// control variable to test if dragging option is available
+var drag = false;
+var  x1, x2, scrollLeft;
 
-slider.onmousedown = function () {
+slider.onmousedown = function (e) {
+	drag = true;
 	slider.classList.add('active');
+	x1 = e.pageX - slider.offsetLeft;
+	console.log(x1);
 }
 
-slider.onmouseup = function () {
+slider.onmouseup = function (e) {
+	drag = false;
 	slider.classList.remove('active');
+}
+
+slider.onmousemove = function(e){
+	if (drag) {
+	x2 = e.pageX - slider.offsetLeft;
+	var pace = (x2 - x1);
+	slider.scrollLeft = slider.scrollLeft - pace;
+	} else { return }
 }
