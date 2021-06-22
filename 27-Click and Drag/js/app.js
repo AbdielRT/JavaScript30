774e7f770f0b1f4ffe0642197a64d48f8228a5be
Abdiel <polito.abs@gmail.com>
2021-06-22 10:35:47 +0200

    .js commented

--- 27-Click and Drag/js/app.js
+++ 27-Click and Drag/js/app.js
@@ -1,24 +1,30 @@
 const slider = document.querySelector('.items');
-// control variable to test if dragging option is available
-var drag = false;
-var  x1, x2, scrollLeft;
+var drag = false;  // control variable to test if drag mode is active
+var  x1, x2;
 
+// As mouse clicks, sets drag variable to true, adds class active for the
+// css effects and saves horizontal position of click 
 slider.onmousedown = function (e) {
 	drag = true;
 	slider.classList.add('active');
-	x1 = e.pageX - slider.offsetLeft;
-	console.log(x1);
+	x1 = e.pageX;
 }
-
+// On mouse click release, returns drag to false and removes css effects
 slider.onmouseup = function (e) {
 	drag = false;
 	slider.classList.remove('active');
 }
 
+// As mouse moves and...
 slider.onmousemove = function(e){
+// mouse click is pressed:	
 	if (drag) {
-	x2 = e.pageX - slider.offsetLeft;
-	var pace = (x2 - x1);
-	slider.scrollLeft = slider.scrollLeft - pace;
+// saves current mouse position 
+	x2 = e.pageX;
+// computes position relative to initial click
+	var delta = (x2 - x1);
+// scrolls horizontally by the delta of clicks position in pixels
+	slider.scrollLeft = slider.scrollLeft - delta;
+	console.log(delta, slider.scrollLeft);
 	} else { return }
 }
